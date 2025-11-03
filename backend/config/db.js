2867a84599ed7b0996752320063dd03282ba1933
Nurdaulet20006 <botakk1667@gmail.com>
2025-11-03 09:28:06 +0500

    обновил index

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://240118003_db_user:P8QYPRT8@cluster0.tipcuf3.mongodb.net/?appName=Cluster0")
+  await mongoose.connect("mongodb+srv://240118003_db_user:Nurik2012@cluster0.tipcuf3.mongodb.net/?appName=Cluster0")
     .then(() => { console.log("DB connected") })
     .catch((err) => { console.log("DB connection error:", err) });
 };
